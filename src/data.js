57c168b49247bcbc41468cbecf740fa36b87439a
Montse2018 <anmmtz@outlook.com>
2018-07-02 13:05:01 -0500

    nueva actualizacion

--- src/data.js
+++ src/data.js
@@ -1,217 +1,61 @@
-//import { futimes } from "fs";
 
-//import { log } from "util";
-//let url = "https://api.myjson.com/bins/17ewfi";
-
-window.get = async () => {
-    const url = "https://api.myjson.com/bins/17ewfi";
-    const thenData = await fetch(url);
-    laboratoria = await thenData.json();
-}
-
-window.data = {
-    //let contenido = document.querySelector('#contenido')
-    computeStudentsStats: () => {
-        /*fetch('https://api.myjson.com/bins/17ewfi')
-        .then(res => res.json())
-        .then(data => {
-            contenido.innerHTML = `
-            <p>Sede: ${data.lima}<p>
-            `
-        })
-    }*/
-    },
-    computeGenerationStats: () => {
-        /*let contenido = document.querySelector('#contenido')
-        computeStudentsStats: () => {
-            fetch('https://api.myjson.com/bins/17ewfi')
-            .then(res => res.json())
-            .then(data => {
-                contenido.innerHTML = `
-                <p>Generacion: ${data.lima.generacion}<p>
-                `
-        })
-    }*/
-    },
-    sortStudents: () => {
-
-    },
-    filterStudents: () => {
-
-    },
-};
-
-/*window.data = {
-
-    getJson: () => {
-        let dataVen = [];
-        let dataGenerations = [];
-        let dataStudents = [];
-        let api = "https://api.myjson.com/bins/17ewfi";
-
-        fetch(api).then(function(dataGenerations){
-            return dataGenerations.json();
-        }).then(function(dataVenues){
-            let i=0;
-            for(let valor of dataVenues.mexico.generacion.cuarta.estudiantes){
-                dataVen[i] = valor;
-                i++;
+        /*window.data = {
+        API :() =>{
+            let laboratoria ="https://api.myjson.com/bins/17ewfi";
+            fetch(laboratoria).then(response => {
+                return response.json();
+                console.log(response.json)
+            }).then(computeGenerationStats => {
+                let sedes = '';
+                //let generacion = '';
+                sedes = Object.getOwnPropertyNames(computeGenerationStats);
+                for(i=0; i< sedes.length; i++){
+                    console.log(sedes[i])
+                    //sedes = sedes[i];
+                    const generacion = '';
+                    for(j=0; j< generacion.length; j++){
+                        console.log(generacion[j])
+                }
+                //let generacion = '';
+                /*sedes = sedes[i];
+                const generacion = Object.getOwnPropertyNames(computeGenerationStats);
+                for(j=0; j< generacion.length; j++){
+                    console.log(generacion[j])
             }
-            console.log(dataVen);
-            return dataVen;
         })
     }
 };*/
-    
-/*
-    function cargarTXT() {
-        fetch(url)
-        .then(function(res) {
-            return res.text();
-        })
-        .then(function(laboratoria) {
-            document.getElementById('resultado').innerHTML = laboratoria;
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
-    }
 
-    function cargarJSON() {
-        fetch('request')
-        .then(function(res) {
-            return res.json();
-        })
-        .then(function(data) {
-            let html = '';
-            data.forEach(function(laboratoria) {
-                html += ´
-                <li>${laboratoria.lima} $ {laboratoria.generacion}</li>
-                        ´;
-            })
-            document.getElementById('resultado').innerHTML = html;
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
-    }
-};*/
+window.computeStudentsStats = (laboratoria) => {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//let jsonResponse 
-//se almacen la URL del JSON que se quiere recuperar en una variable
-//let request = `https://raw.githubusercontent.com/Montse2018/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json`
-//se crea una nueva instancia de objeto de solicitud desde el constructor XMLHttpRequest
-//let requestObject = newXMLHttpRequest ();
-//se utiliza el metodo open para abrir una nueva solicitud que toma el parametro GET (para traer datos)
-//y el parametro requestURL que realiza la solicitud
-//requestObject.open('GET', requestObjectURL);
-//el responseType es para que XHR sepa que el servidor estara retornando al JSON y que debe convertir
-//en un segundo plano en un objeto JavaScript
-//requestObject.responseType = 'json';
-//se utiliza el metodo send para enviar la solicitud
-//requestObject.send();
-
-/*let generation = "";
-//let datosLaboratoria = "";
-//let laboratoria = "";*/
-
-/*window.get = async () => {
-    const request = `https://raw.githubusercontent.com/Montse2018/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json`
-    const thenData = await fetch(request);
-    laboratoria = await thenData.json();
 }
 
-window.data = {
-    computeStudentsStats: () =>{
+window.computeGenerationsStats = (laboratoria) => {
+    const generationsArray = [];
+    const obj = {
+        campus: '',
+        generation: '',
+        average: 0,
+        count: 0,
+    };
+    let average = 0;
+    for (key in laboratoria) {
+        obj.campus = key;
+        average = 0;
+        const generations = Object.keys(laboratoria[key].generacion);
+        generations.forEach((generation) => {
+            obj.generation = generation;
+            const students = laboratoria[key].generacion[generation].estudiantes;
+            for (student in students) {
+                average += students[student].progreso.porcentajeCompletado;
+                average = average / students.length;
+                obj.average = average;
+                obj.count = students.length;
+                generationsArray.push(obj);
 
-    },
-    computeGenerationStats: () =>{
-
-    },
-    sortStudents: () => {
-
-    },
-    filterStudents: () => {
-
-    },
-};*/
-    
-
-    /*computeGenerationStats: () => {
-        const data = JSON.parse(request.responseType);
-        console.log(data.lima.generacion);
-        //let generation = (lima.generacion);
-        //console.log(request.responseType);
-    }
-    asyncRequestObject.onload = computeGenerationStats;
-    // {
-       // let generation = request.response;
-    }*/
-    
-    //generation: document.querySelector("#generacion");
-    //datosLaboratoria: "",
-    //laboratoria: "",
-    //console.log(request);
-
-    /*computeGenerationStats: function() {
-        fetch(request).then(function(laboratoria){
-            console.log(laboratoria.json);
-            return laboratoria.json();
-        }).then(function(datosLaboratoria){
-            let generaciones = datosLaboratoria[0].lima.generacion;
+            }
         })
 
-            /*console.log(laboratoria.json);}).then(function(datosLaboratoria){
-                console.log(laboratoria.lima.generacion);
-                let generation = laboratoria.lima.generacion;
-                generation.innerHTML = generationAPI;})*/
-// }
-//  };*/
-
-/*window.data = {
-let data = {
-    generacion: ["cuarta", "quinta", "tercera"],
-    sedes: [lima, mexico, santiago],
-};
-
-computeGenerationStats(data) {
-    let generation = data.generacion;
+    }
+    return generationsArray;
 }
-console.log(data.generacion);
-
-};*/
-
-            //console.log(laboratoria.generacion[0]);
-            //let generation = laboratoria.generacion[0];
-        
-            //console.log(laboratoria.generacion[0]);
-            //let generation = laboratoria.generacion[0];
-       
--- src/main.js
+++ src/main.js
@@ -1,40 +1,97 @@
-/*document.getElementById('sedesBtn').addEventListener('click', cargarsedes);
-document.getElementById('generacionesBtn').addEventListener('click', cargargeneraciones);
-document.getElementById('estudiantesBtn').addEventListener('click', cargarestiantes);*/
+let buttonSedes = document.getElementById("btnSedes");
+let buttonGeneracion = document.getElementById("btnGeneracion");
 
-//const url = "https://api.myjson.com/bins/17ewfi";
-let contenido = document.querySelector('#contenido')
-
-
-
-
-
-
-
-
-
-
-/*let generation = document.getElementById("generacion");
-let computeGenerationStats = document.getElementById("generation");
-let generation = document.getElementById("#generacion");
-//generation = document.getElementsByClassName("Generaciones");
-let sedes = document.getElementsByClassName("Sedes");
-let buttonSedes = document.getElementById("Sedes");
-let buttonGeneraciones = document.getElementById("Generaciones");
-let btn = document.getElementById("#button");
-//let generation = "";
-let datosLaboratoria = "";
-let laboratoria = "";
-
-btn.addEventListener("click", function(){
-    computeGenerationStats();
+buttonSedes.addEventListener ("click",  () => {
+    window.data.laboratoria()
 })
 
-buttonGeneraciones.addEventListener ("click", event => {
-    window.data.computeGenerationStats (laboratoria, datosLaboratoria)
-    //generation.innerHTML = data[0].generacion;
+buttonGeneracion.addEventListener ("click", () => {
+    window.data.generacion()
 })
 
-buttonSedes.addEventListener ("click", event => {
-    window.data.Sedes () 
-})*/
+/*const json = "https://api.myjson.com/bins/17ewfi";
+
+const getData = () => {
+    fetch(json)
+    .then( response => response.json() )
+    .then((res) => {
+        let sedes = '';
+        //let generacion = '';
+        sedes = Object.getOwnPropertyNames(computeGenerationStats);
+        for(i=0; i< sedes.length; i++){
+            console.log(sedes[i])
+        //const generations = computeGenerationsStats(res);
+        //const users = computeStudentsStats(res);
+        //drawCampus(generations);
+        let buttonGeneracion = document.getElementById("btnGeneracion").addEventListener("click", () => {
+            window.data.API() 
+        })
+    }
+    //.catch((error) => {
+        //console.log(error);
+    })
+};*/
+
+/*let laboratoria ="https://api.myjson.com/bins/17ewfi";
+    let xhttp = newXMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+        let respuesta = JSON.parse(xhhttp.responseText);
+        let laboratori = respuesta.laboratoria;
+        let salida = '';
+        let sedes = '';
+        sedes = Object.getOwnPropertyNames(computeGenerationStats);
+        for(let i=0; i <laboratoria.length; i++) {
+            salida += $(laboratoria[i].sedes)
+        }
+        let buttonSedes = document.getElementById("btnSedes").innerHTML = salida;
+    }
+    }
+    xhttp.open("GET", "laboratoria.json", true);
+    xhhttp.send();*/
+
+   /*fetch(laboratoria).then(response => {
+        return response.json();
+    }).then(datos => {
+        if(this.readyState == 4 && this.status == 200)
+        let respuesta = respose.json
+    })*/
+
+    let laboratoria ="https://api.myjson.com/bins/17ewfi";
+    fetch(laboratoria).then(response => {
+        return response.json();
+        console.log(response.json)
+    }).then(computeGenerationStats => {
+        let sedes = '';
+        //let generacion = '';
+        sedes = Object.getOwnPropertyNames(computeGenerationStats);
+        for(i=0; i< sedes.length; i++){
+            console.log(sedes[i])
+            //sedes = sedes[i];
+            let generacion = '';
+            for(j=0; j< generacion.length; j++){
+                console.log(generacion[j])
+            }
+        }
+    });
+
+
+/*getData();
+
+const drawCampus = (data) => {
+    const sedes = Object.keys(data);
+
+    const containerCampus = document.getElementById('btnSedes');
+    sedes.forEach((sede) => {
+        const option = document.createElement('option');
+        option.innerHTML = sede;
+        containerCampus.appendChild(option);
+    });
+
+    containerCampus.addEventListener('change', iteratorGenerations);
+};
+
+
+/*const drawGenerations = (e) => {
+    //computeGenerationsStats(laboratoria)
+}*/
