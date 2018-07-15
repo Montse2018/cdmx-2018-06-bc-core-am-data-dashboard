4ba16533af7dad28599eb32ef02edc548f44c0a2
Montse2018 <anmmtz@outlook.com>
2018-07-14 22:33:13 -0500

    actualizando html y css

--- src/data.js
+++ src/data.js
@@ -1,4 +1,3 @@
-
 const laboratoria = "https://api.myjson.com/bins/17ewfi";
 
 fetch(laboratoria)
@@ -11,11 +10,11 @@ fetch(laboratoria)
     renderInfo(data);
     renderInfo2(data);
     renderInfo3(data);
-    //renderInfo4(data);
+    renderInfo4(data);
 })
 
-window.data = {
-    coumputeStudentsStats: (laboratoria) => {
+
+window.coumputeStudentsStats = (laboratoria) => {
         const studentsArray = [];
         const student = {
             name: '',
@@ -35,87 +34,8 @@ window.data = {
                     }
                 }
             }
-        }
+        };
     }
-};
-   /* computeGenerationStats: (laboratoria) => {
-        const newArray = [],
-        const generacion = {
-            campus: '',
-            generation: '',
-            average: 0,
-            count: 0,
-        }
-    },
-
-    sortStudents: (laboratoria) => {
-    const sort = [];
-    sortStudent = {
-        students: '',
-        orderBy: '',
-        orderDirection: '',
-    };
-},
-
-    filterStudents: (laboratoria) => {
-    const filterSt = [];
-    filterStudent = {
-        students: '',
-        search: '',
-    }
-};
-        /*if (data.hasOwnProperty(key)) {
-            const element = data[key];
-            console.log(key, element)
-            console.log(data.generacion)
-        }
-    }
-
-}
-
-window.computeGenerationStats = (laboratoria) => {
-    fetch(HTMLLabelElement)
-    .then(response => response.json())
-    .then(data => {
-
-    })
-}*/
-
-
-
-
-
-/*window.computeGenerationStats = (laboratoria) => {
-    const generationArray = [];
-            for(const key in data) {
-                if (data.hasOwnProperty(key)) {
-                    const element = data[key];
-                    console.log(key, element) //crear variables (sedes, generacion, estudiantes...)
-                    console.log(element.generacion.cuarta.estudiantes)
-                }
-
-            }
-
-    })
-}
-}*/
-
-/*window.computeStudentsStats = (laboratoria) => {
-
-computeStudentsState(poner elementos)
-}
-
-window.computeGenerationsStats = (laboratoria) => {
-    const laboratoria = "https://api.myjson.com/bins/17ewfi";
-    fetch(laboratoria)
-    .then(res => res.json())
-    .then(data => {
-        const sedes = '';
-        sedes = Object.getOwnPropertyNames(data);
-
-    })
-
-}*/
 
 window.sortStudents = (laboratoria) => {
     const sort = [];
